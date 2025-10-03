b008d143ec755cbf183fd56f51b876da24fa674b
Ricardo <ricardoprogramador1@gmail.com>
2025-10-03 10:50:05 -0500

    Update in MonsterDAO class.

--- src/DBConnection/MonsterDAO.java
+++ src/DBConnection/MonsterDAO.java
@@ -2,10 +2,7 @@ package DBConnection;
 
 import model.Monster;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,4 +37,34 @@ public class MonsterDAO {
         }
         return monsters;
     }
+
+    public Monster getMonster(int id){
+        String sql = "SELECT * FROM monster WHERE id_monster = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                Monster monster = new Monster();
+                monster.setId_monster(rs.getInt("id_monster"));
+                monster.setName(rs.getString("name"));
+                monster.setLevelMonster(rs.getInt("levelMonster"));
+                monster.setGiveXP(rs.getDouble("giveXP"));
+                monster.setAttack(rs.getDouble("attack"));
+                monster.setDefense(rs.getDouble("defense"));
+                monster.setHp(rs.getDouble("hp"));
+                monster.setStrength(rs.getInt("strength"));
+                monster.setIntelligence(rs.getInt("intelligence"));
+                monster.setWisdom(rs.getInt("wisdom"));
+                monster.setLucky(rs.getInt("lucky"));
+//                monster.setMana(rs.getDouble("mana"));
+//                monster.setMaximumMana(rs.getDouble("maximunmana"));
+                return monster;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
 }
